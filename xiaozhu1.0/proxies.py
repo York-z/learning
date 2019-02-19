2b627003e9b153a8d8d6e5a7028ff15b90e1a42e
York-z <45679954+York-z@users.noreply.github.com>
2019-02-19 17:56:56 +0800

    Update proxies.py

--- xiaozhu1.0/proxies.py
+++ xiaozhu1.0/proxies.py
@@ -5,7 +5,8 @@ import requests
 import random
 
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36' +
+           ' (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36'}
 
 
 def get_ip_list(url):
